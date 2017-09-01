53f4e3d88639072ec7b7e2dee1c1e5a885cab2c8
Howard Deiner <howard.deiner@deinersoft.com>
2017-09-01 17:03:31 -0400

    Initial commit.

--- src/main/java/com/deinersoft/timeteller/TimeTeller.java
+++ src/main/java/com/deinersoft/timeteller/TimeTeller.java
@@ -50,25 +50,154 @@ public class TimeTeller {
                 }
                 break;
             case 2:
-                String[] s1 = {"twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"};
-                String[] s2 = {"", "almost ten after", "ten after", "a quarter after", "twenty after", "almost half past", "half past", "almost twenty before", "twenty before", "a quarter of", "ten of", "almost"};
-                String[] s3 = {"at night", "in the morning", "in the afternoon", "in the evening"};
+                result = "";
 
                 if (k >= 30) j++;
 
-                if (j >= 3) {
-                    result += s2[(j+2)/5] + " ";
-                }
-                if (j < 35) {
-                    result += s1[i%12];
-                }  else {
-                    result += s1[(i+1)%12];
+                if (j <= 3) {
+                    result = result + "about ";
+                } else if (j <= 8) {
+                    result = result + "a little after ";
+                } else if (j <= 13) {
+                    result = result + "almost ten after ";
+                } else if (j <= 18) {
+                    result = result + "about ten after ";
+                } else if (j <= 23) {
+                    result = result + "about a quarter after ";
+                } else if (j <= 23) {
+                    result = result + "about twenty after ";
+                } else if (j <= 28) {
+                    result = result + "almost half after ";
+                } else if (j <= 33) {
+                    result = result + "about half past ";
+                } else if (j <= 38) {
+                    result = result + "almost twenty before ";
+                } else if (j <= 43) {
+                    result = result + "about twenty before ";
+                } else if (j <= 48) {
+                    result = result + "about a quarter of ";
+                } else if (j <= 53) {
+                    result = result + "about ten of ";
+                } else if (j <= 58) {
+                    result = result + "almost ";
+                } else {
+                    result = result + "about ";
                 }
 
-                result += " " + s3[i/6];
+                if (j < 35) {
+                    if (i == 0) {
+                        result = result + "twelve ";
+                    } else if (i == 1) {
+                        result = result + "one ";
+                    } else if (i == 2) {
+                        result = result + "two ";
+                    } else if (i == 3) {
+                        result = result + "three ";
+                    } else if (i == 4) {
+                        result = result + "four ";
+                    } else if (i == 5) {
+                        result = result + "five ";
+                    } else if (i == 6) {
+                        result = result + "six ";
+                    } else if (i == 7) {
+                        result = result + "seven ";
+                    } else if (i == 8) {
+                        result = result + "eight ";
+                    } else if (i == 9) {
+                        result = result + "nine ";
+                    } else if (i == 10) {
+                        result = result + "ten ";
+                    } else if (i == 11) {
+                        result = result + "eleven ";
+                    } else if (i == 12) {
+                        result = result + "twelve ";
+                    } else if (i == 13) {
+                        result = result + "one ";
+                    } else if (i == 14) {
+                        result = result + "two ";
+                    } else if (i == 15) {
+                        result = result + "three ";
+                    } else if (i == 16) {
+                        result = result + "four ";
+                    } else if (i == 17) {
+                        result = result + "five ";
+                    } else if (i == 18) {
+                        result = result + "six ";
+                    } else if (i == 19) {
+                        result = result + "seven ";
+                    } else if (i == 20) {
+                        result = result + "eight ";
+                    } else if (i == 21) {
+                        result = result + "nine ";
+                    } else if (i == 22) {
+                        result = result + "ten ";
+                    } else if (i == 23) {
+                        result = result + "eleven ";
+                    }
+                } else {
+                    if (i == 0) {
+                        result = result + "one ";
+                    } else if (i == 1) {
+                        result = result + "two ";
+                    } else if (i == 2) {
+                        result = result + "three ";
+                    } else if (i == 3) {
+                        result = result + "four ";
+                    } else if (i == 4) {
+                        result = result + "five ";
+                    } else if (i == 5) {
+                        result = result + "six ";
+                    } else if (i == 6) {
+                        result = result + "seven ";
+                    } else if (i == 7) {
+                        result = result + "eight ";
+                    } else if (i == 8) {
+                        result = result + "nine ";
+                    } else if (i == 9) {
+                        result = result + "ten ";
+                    } else if (i == 10) {
+                        result = result + "eleven ";
+                    } else if (i == 11) {
+                        result = result + "twelve ";
+                    } else if (i == 12) {
+                        result = result + "one ";
+                    } else if (i == 13) {
+                        result = result + "two ";
+                    } else if (i == 14) {
+                        result = result + "three ";
+                    } else if (i == 15) {
+                        result = result + "four ";
+                    } else if (i == 16) {
+                        result = result + "five ";
+                    } else if (i == 17) {
+                        result = result + "six ";
+                    } else if (i == 18) {
+                        result = result + "seven ";
+                    } else if (i == 19) {
+                        result = result + "eight ";
+                    } else if (i == 20) {
+                        result = result + "nine ";
+                    } else if (i == 21) {
+                        result = result + "ten ";
+                    } else if (i == 22) {
+                        result = result + "eleven ";
+                    } else if (i == 23) {
+                        result = result + "twelve ";
+                    }
+                }
+
+                if ((i >=0) && (i <= 5)) {
+                    result = result + "at night";
+                } else if ((i >=6) && (i<=11)) {
+                    result = result + "in the morning";
+                } else if ((i >=12) && (i<=17)) {
+                    result = result + "in the afternoon";
+                } else if ((i >=18) && (i<=23)) {
+                    result = result + "in the evening";
+                }
 
                 if (whichOne == 2) {
-                    result += " Zulu";
+                    result = result + " Zulu";
                 }
 
                 break;
